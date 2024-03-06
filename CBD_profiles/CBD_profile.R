9afd603c9d847ae20f6a0030bfcb2fb8f78f8932
cteurnier <cteurnier@Mac-Studio-de-fpimont.local>
2024-03-06 14:41:51 +0100

    settings for subsample

--- CBD_profiles/CBD_profile.R
+++ CBD_profiles/CBD_profile.R
@@ -5,12 +5,46 @@ library(ggthemes)
 
 ?pixel_metrics
 
-source("C:/Users/ateurnier/OneDrive - INRAE SharePoint SE/CDD/carto/fCBDprofile_fuelmetrics.R")
+source("~/Documents/lidarForFuel/CBD_profiles/fCBDprofile_fuelmetrics.R")
+
+
 
 las = readLAS("D:/placettes_tot_pretraite/44.laz")
 test = pixel_metrics(las, fCBDprofile_fuelmetrics, 20)
 
+
+catalog_pleiade = catalog(c("~/Documents/subsample_pretreated/1.laz", "~/Documents/subsample_pretreated/2.laz"))
+# set the number of workers
+plan(sequential)
+plan(multisession, workers = 8L)
+
+# Show progression  (8 hours for luberon => 400km²)
+opt_progress(catalog_pleiade)=T
+
+opt_stop_early(catalog_pleiade)=T
+# Keep the size of the tiles (1km²)
+opt_chunk_size(catalog_pleiade)=0
+
+# Do not use a Buffer, no need and slows down the process
+opt_chunk_buffer(catalog_pleiade) <- 0
+
+opt_laz_compression(catalog_pleiade) = T
+
+# Where to output the rasters
+opt_output_files(catalog_pleiade) <- "~/Documents/{ID}" # chemin vers le dossier de sortie des nouveau quadras
+
+
+
+# catalog_apply(catalog_pleiade,Fuel_metrics_4_Map_FireEURisk)
+
+pixel_metrics(catalog_pleiade,~fCBDprofile_fuelmetrics(X=X,Y=Y,Z=Z,Zref = Zref,Easting = Easting ,Northing = Northing,Elevation = Elevation ,LMA = LMA,threshold = 0.02,WD = 591 ,scanning_angle = T,limit_flyheight = 800,limit_N_points = 400,datatype = "Pixel",omega = 0.77,d=0.5,G = 0.5),res=20)
+
+opt_select(catalog_pleiade)
+
+
 Metric_20=pixel_metrics(las,~fCBDprofile_fuelmetrics(limit_flyheight = 500,X=X,Y=Y,Z=Z,Zref = Zref,Easting = Easting ,Northing = Northing,Elevation = Elevation ,LMA = LMA,threshold = 0.012,WD = 575 ,limit_N_points = 400,datatype = "Pixel",omega = 0.7,d=0.5,G = 0.5),res=20)
+
+
 
 X = las$X
 Y = las$Y
