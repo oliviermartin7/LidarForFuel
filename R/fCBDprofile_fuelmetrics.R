054b2ed5ec7bb7db55bc3f3e6756dd01012e841b
Olivier MARTIN <omartin@cirad.fr>
2025-05-22 16:01:17 +0200

    - add sd - change variable name (hist_z -> Ni; N) - change NRD to NRDc (Ni+1/N+2)

--- R/fCBDprofile_fuelmetrics.R
+++ R/fCBDprofile_fuelmetrics.R
@@ -77,9 +77,11 @@ fCBDprofile_fuelmetrics=function(datatype="Pixel",X,Y,Z,Zref,ReturnNumber,Eastin
   ## Create a sequence to make strata  ----
   seq_layer=c(min(Z),seq(0,max(Z),d),max(Z))
   ## hist to get number of return in strata  ----
-  hist_z=hist(Z,breaks=seq_layer,plot=F)$count
+  Ni=hist(Z,breaks=seq_layer,plot=F)$count
+  N=cumsum(Ni)
   ## NRD estimation  ----
-  NRD=hist_z/cumsum(hist_z)
+  # Ni +1 et N +2 pour les cas où Ni=0 ou Ni=1 => NRDc de l'equations 23 et 24 de Pimont et al 2018
+  NRD=(Ni+1)/(N+2)
   ## Gap fraction estimation ----
   Gf=1-NRD
 
@@ -116,7 +118,8 @@ fCBDprofile_fuelmetrics=function(datatype="Pixel",X,Y,Z,Zref,ReturnNumber,Eastin
   omega=omega # Clumping factor. 1= Random distribution = < 1 = clumped
   ## Plant area density calculation (actually FAD --> fuel area density: leaves + twigs) ----
   PAD=-(log(Gf)*cos_theta/(G*omega)/d)
-  Var_PAD=(PAD^2/(NRD))/cumsum(hist_z)
+  Var_PAD=(PAD^2/(NRD))/(N+2)
+  SD_PAD  = 2/(N+2) * sqrt (NRD/(1-NRD))
   ### LMA from g/cm² to kg.m2
   LMA=mean(LMA,na.rm=T)/1000
 
@@ -143,7 +146,7 @@ fCBDprofile_fuelmetrics=function(datatype="Pixel",X,Y,Z,Zref,ReturnNumber,Eastin
   seq_layer=seq_layer[-c(1,length(seq_layer))]+(d/2)
 
   ## Table with strata height PAD and CBD ----
-  PAD_CBD_Profile=data.table::data.table(cbind(H=seq_layer),PAD=PAD[-1],CBD=CBD[-1],Var_PAD=Var_PAD[-1],NRD=NRD[-1],Ni=hist_z[-1],N=cumsum(hist_z)[-1])
+  PAD_CBD_Profile=data.table::data.table(cbind(H=seq_layer),PAD=PAD[-1],CBD=CBD[-1],Var_PAD=Var_PAD[-1],NRD=NRD[-1],Ni=Ni[-1],N=N[-1])
 
   # 2. Work on profile to get FPT and fuel metrics ----
 
