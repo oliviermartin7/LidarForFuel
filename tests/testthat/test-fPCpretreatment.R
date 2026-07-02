199ed88429c6dd9a50e7dc1cf761dff8f74000d8
Olivier Martin <49266666+oliviermartin7@users.noreply.github.com>
2026-07-02 14:38:22 +0200

    Revert "33 cos theta value is na in function pad metrics with arg scanning angle false"

--- tests/testthat/test-fPCpretreatment.R
+++ tests/testthat/test-fPCpretreatment.R
@@ -1,4 +1,5 @@
 test_that("filter_season", {
+  plot_hist_days <- FALSE
   ref <- as.POSIXct("2011-09-14 01:46:40", tz = "UTC")
   gpstime <- seq.Date(as.Date("2023-07-01"), as.Date("2023-08-31"), by = "days")
   gpstime <- as.POSIXct(gpstime) - ref
@@ -10,18 +11,21 @@ test_that("filter_season", {
 
   # here returned las should be full
   months <- 5:10
-  las1 <- lidR::filter_poi(las, filter_gpstime(gpstime, months = months))
+  las1 <- filter_seasons(las, months, plot_hist_days = plot_hist_days)
   expect_true(nrow(las1@data) == nrow(las@data))
 
   # returned las should be half size
   months <- 7
-
-  las1 <- lidR::filter_poi(las, filter_gpstime(gpstime, months = months))
-  expect_true(nrow(las1@data) == nrow(las@data) / 2)
+  expect_warning({
+    las1 <- filter_seasons(las, months, plot_hist_days = plot_hist_days)
+    expect_true(nrow(las1@data) == nrow(las@data) / 2)
+  })
 
   # returned las should be half empty
   months <- 1:5
-  las1 <- lidR::filter_poi(las, filter_gpstime(gpstime, months = months))
+  expect_warning({
+    las1 <- filter_seasons(las, months, plot_hist_days = plot_hist_days)
+  })
   expect_true(nrow(las1@data) == 0)
 })
 
@@ -37,25 +41,24 @@ test_that("filter_date_mode", {
 
   deviation_days <- 2
 
-  las1 <- lidR::filter_poi(
-    las,
-    filter_gpstime(
-      gpstime,
+  expect_warning({
+    las <- filter_date_mode(
+      las,
       deviation_days = deviation_days,
-      gpstime_ref = "2023-01-01 00:00:00"
+      gpstime_ref = "2023-01-01 00:00:00",
+      plot_hist_days = FALSE
     )
-  )
-  expect_true(nrow(las1@data) == deviation_days * 2 + N + 1)
+  })
+  expect_true(nrow(las@data) == deviation_days * 2 + N + 1)
 
   # test where only one date is present, should not filter any point
   data[["gpstime"]] <- runif(nrow(data)) * 3600 * 24
   las <- lidR::LAS(data)
-  las <- lidR::filter_poi(
+  las <- filter_date_mode(
     las,
-    filter_gpstime(
-      gpstime, deviation_days = deviation_days,
-      gpstime_ref = "2023-01-01 00:00:00"
-    )
+    deviation_days = deviation_days,
+    gpstime_ref = "2023-01-01 00:00:00",
+    plot_hist_days = FALSE
   )
   expect_true(nrow(las@data) == nrow(data))
 })
@@ -66,23 +69,15 @@ test_that("fPCpretreatment", {
   # LMA value selected = 120.6 that is the LMA for Pinus halepensis, the
   # dominant species of the plot
   expect_warning(
-    m30_font_blanche_pretreated <- fPCpretreatment(path2laz)
+    m30_font_blanche_pretreated <- fPCpretreatment(path2laz, LMA = 120.6)
   )
   # displaying the new attributes in the las
   expected_names <- c(
-    "Easting", "Northing", "Elevation", "Time", "Zref"
+    "Easting", "Northing", "Elevation", "Time", "LMA", "WD", "Zref"
   )
   expect_contains(names(m30_font_blanche_pretreated), expected_names)
-
-  # write and read to check that the attributes are still there
-  outdir <- withr::local_tempdir(pattern = "lidarforfuel") # , .local_envir = globalenv()
-  expect_true(file.exists(outdir))
-  tmpfile <- file.path(outdir, "m30_font_blanche_pretreated.laz")
-  lidR::writeLAS(m30_font_blanche_pretreated, tmpfile)
-  m30_font_blanche_pretreated_read <- lidR::readLAS(tmpfile)
-  expect_contains(names(m30_font_blanche_pretreated_read), expected_names)
-
 })
+
 
 
 # test_that("fPCpretreatment-full-tile", {
--- tests/testthat/test-traj.R
+++ tests/testthat/test-traj.R
@@ -1,31 +1,10 @@
 test_that("lasrmdup", {
   las_file <- system.file("extdata", "example.laz", package = "rlas")
   las <- lidR::readLAS(las_file)
-  # with duplicate returns
-  las@data$ReturnNumber <- 1
+  las@data$ReturnNumber <- 1 # make duplicates
   las1 <- lasrmdup(las)
+
   expect_true(nrow(las) > nrow(las1))
-
-  # with duplicated pulses
-  las <- lidR::readLAS(las_file)
-  # make a fake duplicated pulse, using different ReturnNumber to make sure it is not the duplicate returns filter
-  # that removes points
-  las@data <- las@data[1:3, `:=`(ReturnNumber = 1:3, NumberOfReturns = 3, UserData = 0, gpstime = las$gpstime[1])]
-  las@data <- las@data[4:6, `:=`(ReturnNumber = 4:6, NumberOfReturns = 6, UserData = 0, gpstime = las$gpstime[1])]
-  las1 <- lasrmdup(las)
-  expect_true(nrow(las) == nrow(las1) + 6)
-
-  # with more returns then NumberOfReturns
-  las@data <- las@data[1:6, `:=`(ReturnNumber = c(1:5, 5), NumberOfReturns = 5, UserData = 0, gpstime = las$gpstime[1])]
-  las1 <- lasrmdup(las)
-  expect_true(nrow(las) == nrow(las1) + 6)
-
-  # However, in case of the same number of returns by pulse,
-  # we cannot detect it
-  las@data <- las@data[1:3, `:=`(ReturnNumber = 1:3, NumberOfReturns = 6, UserData = 0, gpstime = las$gpstime[1])]
-  las@data <- las@data[4:6, `:=`(ReturnNumber = 4:6, NumberOfReturns = 6, UserData = 0, gpstime = las$gpstime[1])]
-  las1 <- lasrmdup(las)
-  expect_false(nrow(las) == nrow(las1) + 6)
 })
 
 test_that("lasrenumber", {
@@ -51,39 +30,13 @@ test_that("get and add traj", {
   # when not enough pulses to reconstruct
   las1 <- lidR::LAS(las)
   las1@data <- las1@data[1:50, ]
-  expect_error(
-    {
-      traj1 <- get_traj(las1)
-    },
-    regexp = "Computed trajectory is empty",
-    fixed = TRUE
-  )
-
-  # what happens if any gpstime=0
-  las2 <- lidR::readLAS(path2laz)
-  las2@data <- las2@data[gpstime %in% unique(las2@data$gpstime)[1:1e4], gpstime := 0][]
-  # no error
-  expect_warning(
-    {
-      traj2 <- get_traj(las2)
-    },
-    "Only one sensor position was found for PointSourceID 49",
-    fixed = TRUE
-  )
-  expect_true(all(traj2$gpstime != 0))
-
-
-  # },
-  # regexp = "Computed trajectory is empty",
-  # fixed = TRUE
-  # )
-
-
-  # expect_true(nrow(traj1) > 0)
+  expect_warning({
+    traj1 <- get_traj(las1)
+  })
+  expect_true(nrow(traj1) > 0)
   # TODO: test multi_pulse
 
   # add trajectory
-  las <- path2laz |> lidR::readLAS()
   las2 <- add_traj_to_las(las, traj)
   expect_contains(names(las2@data), c("Easting", "Northing", "Elevation", "Time"))
 })
